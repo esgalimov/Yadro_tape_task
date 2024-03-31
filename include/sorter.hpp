5d0b324f66ed77a0fa2a003c5b11c8a230eaffd1
Emil Galimov <esgalimov@gmail.com>
2024-03-31 11:39:22 +0300

    sorter start

--- include/sorter.hpp
+++ include/sorter.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "tape.hpp"
 #include <vector>
+#include <algorithm>
 
 namespace tape_sorter {
     namespace cp = config_parser;
@@ -17,20 +18,117 @@ namespace sorter {
 
         std::vector<T> ram_;
 
-        cp::config_t config_;
+        const std::time_t rw_tm_, rewind_tm_ , shift_tm_;
 
         std::time_t sort_time_ = 0;
 
-        // void load_tape_into_ram(tape::tape_t<T>& tp) {
-        //     while (tp.sz_ >= ram)
-        // }
+        using ram_iter = typename std::vector<T>::iterator;
+
+        ram_iter load_tape_into_ram(tape::tape_t<T>& tp, ram_iter start, ram_iter end) {
+            for (start != end && !tp.is_end()) {
+                *start = itape_.read();
+                itape_.shift_next();
+                start++;
+            }
+
+            sort_time += (start - ram_.begin()) * (rw_tm_ + shift_tm_);
+
+            return ++start;
+        }
+
+        ram_iter load_ram_into_tape(tape::tape_t<T>& tp, ram_iter start, ram_iter end) {
+            for (start != end && !tp.is_end()) {
+                itape_.write(*start);
+                itape_.shift_next();
+                start++;
+            }
+
+            sort_time += (start - ram_.begin()) * (rw_tm_ + shift_tm_);
+
+            return ++start;
+        }
+
+        void sort_ram(ram_iter start, ram_iter end) { std::sort(start, end); }
+
+        std::size_t sort_parts_in_ram() {
+            std::size_t i = 0;
+            while (!itape_.is_end()) {
+                ram_iter last_loaded = load_tape_into_ram(itape_, ram_.begin(), ram_.end());
+
+                sort_ram(ram_.begin(), last_loaded);
+
+                if (i++ % 2) load_ram_into_tape(tmp_tp_1_, ram_.begin(), last_loaded);
+                else         load_ram_into_tape(tmp_tp_2_, ram_.begin(), last_loaded);
+            }
+            return tmp_tp_1_.pos();
+        }
+
+        void sort_little_tape_in_ram() {
+            ram_iter last_loaded = load_tape_into_ram(itape_, ram_.begin(), ram_.end());
+
+            sort_ram(ram_.begin(), last_loaded);
+
+            load_ram_into_tape(otape_, ram_.begin(), last_loaded);
+        }
+
+        void merge_tmps(std::size_t tmp_pos_1) {
+            tmp_tp_1_.rewind_begin();
+            tmp_tp_2_.rewind_beign();
+
+            tmp_pos_2 = otape_.size() - tmp_pos_1 - 1;
+
+            while (tmp_pos_1 > 0 && tmp_pos_2 > 0) {
+                T val1 = tmp_tp_1_.read(), val2 = tmp_tp_2_.read();
+
+                if (val1 < val2) {
+                    otape_.write(val1);
+                    tmp_tp_1_.shift_next();
+                    tmp_pos_1--;
+                }
+                else {
+                    otape_.write(val2);
+                    tmp_tp_2_.shift_next();
+                    tmp_pos_2--;
+                }
+                otape_.shift_next();
+            }
+
+            while (tmp_pos_1 > 0) {
+                otape_.write(val1);
+                tmp_tp_1_.shift_next();
+            }
+
+            while (tmp_pos_2 > 0) {
+                otape_.write(val2);
+                tmp_tp_2_.shift_next();
+            }
+
+            sort_time += 2 * rewind_tm_ + 2 * otape_.size() * (rw_tm_ + shift_tm_);
+        }
+
+        void load_merged_into_tmps() {
+
+        }
 
     public:
         sorter_t(const std::string& iname, const std::string& oname, cp::config_t config) :
-            itape_(iname, config.tape_sz_), otape_(oname, config.tape_sz_), config_(config) {
-                ram_.reserve(config_.ram_sz_);
+            itape_(iname, config.tape_sz_), otape_(oname, config.tape_sz_),
+            rw_tm_(config.rw_tm_), rewind_tm_(config.rewind_tm_), shift_tm_(config.shift_tm_) 
+        {
+                ram_.resize(config.ram_sz_ - config.ram_sz_ % sizeof(T));
+
+                auto tmp_path = std::filesystem::temp_directory_path();
+
+                tmp_tp_1_.init_stream(tmp_path.append("tape_task_tmp_1.bin"), config.tape_sz_);
+                tmp_tp_2_.init_stream(tmp_path.append("tape_task_tmp_2.bin"), config.tape_sz_);
         }
 
+        void sort() {
+            if (itape_.size() < ram_.size()) 
+                sort_little_tape_in_ram();
+
+            sort_parts_in_ram();
+        }
         
     };
 } //<-- namespace sorter
--- include/tape.hpp
+++ include/tape.hpp
@@ -5,19 +5,26 @@ namespace tape_sorter {
 namespace tape {
     template <typename T> class tape_buffer_t {
     protected:
-        static constexpr int T_sz_ = sizeof(T);
         std::fstream file_;
         std::size_t  sz_;
 
-        tape_buffer_t(const std::string& filename, std::size_t sz) : 
-            file_(filename, std::ios::binary | std::ios::in | std::ios::out),
-            sz_(sz - sz % T_sz_) {}
+        tape_buffer_t() = default;
 
-        tape_buffer_t(const tape_buffer_t& tp)                   = delete;
+        tape_buffer_t(const std::string& filename, std::size_t sz) : 
+            file_(filename, std::ios::binary | std::ios::in | std::ios::out), sz_(sz) {}
+
+        tape_buffer_t(const tape_buffer_t& tp)            = delete;
         tape_buffer_t& operator=(const tape_buffer_t& tp) = delete;
 
-        tape_buffer_t& operator=(tape_buffer_t&& tp)      = default;
         tape_buffer_t(tape_buffer_t&& tp)                 = default;
+        tape_buffer_t& operator=(tape_buffer_t&& tp)      = default;
+
+        void init_stream(const std::string& filename, std::size_t sz) {
+            file_.close();
+            file_.open(filename, std::ios::binary | std::ios::in | 
+                                 std::ios::out | std::ios::trunc);
+            sz_ = sz;
+        }
 
         ~tape_buffer_t() = default;
     };
@@ -27,27 +34,21 @@ namespace tape {
     class tape_t final : private tape_buffer_t<T> {
         using tape_buffer_t<T>::file_;
         using tape_buffer_t<T>::sz_;
-        using tape_buffer_t<T>::T_sz_;
+        static constexpr int T_sz_ = sizeof(T);
         
     public:
         tape_t() = default;
 
         tape_t(const std::string& filename, std::size_t sz) : 
-            tape_buffer_t<T>(filename, sz) {}
+            tape_buffer_t<T>(filename, sz + T_sz_ - sz % T_sz_) {}
 
-        void create(const std::string& filename, std::size_t sz) {
-            file_.close();
-            file_.open(filename, std::ios::binary | std::ios::in | std::ios::out);
-            sz_ = sz - sz % T_sz_;
+        void init_stream(const std::string& filename, std::size_t sz) {
+            tape_buffer_t<T>::init_stream(filename, sz + T_sz_ - sz % T_sz_);
         }
 
-        void rewind_begin() {
-            file_.seekg(0);
-        }
+        void rewind_begin() { file_.seekg(0); }
 
-        void rewind_end() {
-            file_.seekg(sz_ - T_sz_);
-        }
+        void rewind_end()   { file_.seekg(sz_ - T_sz_); }
 
         T read() {
             T val = 0;
@@ -63,20 +64,23 @@ namespace tape {
         }
 
         void shift_next() {
-            if (file_.tellg() < sz_ - T_sz_)
-                file_.seekg(T_sz_, std::ios_base::cur);
+            if (end_of_tape()) file_.seekg(T_sz_, std::ios_base::cur);
             else throw std::runtime_error(
                 "Error: tape cannot shift next, magnetic head is at the end of tape");
         }
 
         void shift_prev() {
-            if (file_.tellg() != 0)
-                file_.seekg(-T_sz_, std::ios_base::cur);
+            if (begin_of_tape()) file_.seekg(-T_sz_, std::ios_base::cur);
             else throw std::runtime_error(
                 "Error: tape cannot shift prev, magnetic head is at the beginning of tape");
         }
 
         std::size_t size() { return sz_; }
+
+        std::size_t pos()  { return file_.tellg() / T_sz_; }
+
+        bool is_end()   { return file_.tellg() >= sz_ - T_sz_; }
+        bool is_begin() { return file_.tellg() == 0; }
     };
    
 } //<-- namespace tape
